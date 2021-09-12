3426b73a8e7a4d0952bdda61e6dc2806786bae70
tiana25 <tianati01@gmail.com>
2021-09-12 13:00:31 +0300

    promises practice

--- async/index.js
+++ async/index.js
@@ -1,6 +1,6 @@
-console.log('Start')
+// console.log('Start')
 
-console.log('Start 2')
+// console.log('Start 2')
 
 /** Почему поток не блокируется?
  * Почему программа регистрирует это и в нужный момент вызывает?
@@ -16,8 +16,70 @@ console.log('Start 2')
  * то он закидывает ее обратно в стек и выполняет ее
  */
 
-window.setTimeout(function(){
-    console.log('inside timeout, after 2s')
-}, 2000)
+// window.setTimeout(function(){
+//     console.log('inside timeout, after 2s')
+// }, 2000)
 
-console.log('End')
+// console.log('End')
+
+ console.log('Request data...')
+
+// const p = new Promise(function(resolve, reject){
+//     setTimeout(()=>{
+//         console.log('Preparing data ...')
+//         const backenddata = {
+//             server: 'aws',
+//             port:2000,
+//             status: 'working'
+//         }
+//         //Таким образом говорим промису, что он завершил свое выполнение
+//         resolve(backenddata)
+//     }, 2000)
+// })
+
+// p.then(data=>{
+//     return new Promise((resolve, reject) =>{
+//         setTimeout(()=>{
+//             data.modified = true;
+//             resolve(data)
+//             }, 2000)    
+//     })
+// })
+// // .catch(err => console.error('Error: ', err))
+// .then(clientData =>{
+//         console.log('Data received', clientData);
+//         clientData.fromPromise = true
+//         return clientData
+// }).then(data => {
+//     console.log('Modified data',data)
+// })
+// //Если поставим catch в конце, то получим всего одну ошибку
+// .catch(err => console.error('Error: ', err))
+// //Данный метод будет вызван в любом случае в не зависимости от успеха или ошибки
+// .finally(()=>{
+//     console.log('Finally')
+// })
+
+const sleep = ms => {
+    return new Promise(resolve => {
+        setTimeout( () => resolve(), ms)
+    })
+}
+
+// sleep(2000).then( () => console.log('After 2 sec'))
+// sleep(3000).then (() => console.log('After 3 sec'))
+
+//Передаем массив промисов, возвращает промис, который будет выполнен только тогда,
+// когда завершатся все промисы в этом массиве
+
+Promise.all([sleep(2000), sleep(5000)])
+    .then(()=>{
+        console.log('All promises')
+    })
+//Когда выполнится первый промис, то тогда сразу же этот промис в race() отработает
+//Полезно в тех случаях, когда нам нужно определить какой промис был выполнен первым
+Promise.race([sleep(2000), sleep(5000)])
+.then(()=>{
+    console.log('All promises')
+})
+
